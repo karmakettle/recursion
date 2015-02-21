5b808e096f3a4d09ff87efee61eb92f6765bf124
karmakettle <karmakettle@gmail.com>
2015-02-21 02:18:39 -0600

    First four tests passing

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,20 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  if (json === '[]') {
+  	return [];
+  }
+  if (json === '{}') {
+  	return {};
+  }
+  if (json[0] === '{') {
+  	var result = {};
+  	var nextQuoteIdx = json.indexOf("\"", 2);
+  	var thirdQuoteIdx = json.indexOf("\"", nextQuoteIdx + 1);
+  	var lastQuoteIdx = json.indexOf("\"", thirdQuoteIdx + 1)
+  	var key = json.slice(2, nextQuoteIdx);
+  	var val = json.slice(thirdQuoteIdx + 1, lastQuoteIdx);
+  	result[key] = val;
+  	return result;
+  }
 };
